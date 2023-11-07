8d86dfc6f0c9b5be2937083dc825817b1e0b736a
ElisBjorkman <128683649+ElisBjorkman@users.noreply.github.com>
2023-11-07 11:32:39 +0100

    Add files via upload

---
+++ python2/main.py
@@ -0,0 +1,61 @@
+"""
+nummer = 3
+nummer2 = 7
+nummer3 = 9
+
+
+print("hej")
+
+
+nummer = input()
+nummer2 = input()
+
+nummer4 = int(nummer) * int(nummer2)
+
+nummer3 = input()
+
+print(int(nummer4) / int(nummer3))
+"""
+
+
+
+
+
+"""
+nummer = int(input())
+
+if nummer < 10:
+    print("numret är mindre än 10")
+
+
+if nummer > 10:
+    print ("numret är större än 10")
+
+
+if nummer == 10:
+    print("numret är 10")
+
+
+if nummer < 10:
+    print("numret är mindre än 10")
+elif nummer > 10:
+    print ("numret är större än 10")
+elif nummer == 10:
+    print("numret är 10")
+"""
+
+
+print(" minde är 100* eller större än 100/ ")
+
+nummer = int(input())
+nummer2 = int(input())
+
+
+if nummer < 100:
+    print(nummer * nummer2)
+    #minder än 100
+
+
+elif nummer > 100:
+    print (nummer / nummer2)
+    #större än 100
