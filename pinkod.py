d8e64ef0b651b828eb9a580209ceb4469cbbc10b
ElisBjorkman <128683649+ElisBjorkman@users.noreply.github.com>
2023-12-05 10:01:41 +0100

    Add files via upload

---
+++ pinkod.py
@@ -0,0 +1,36 @@
+
+
+pin = input(int)
+
+
+            
+pin_lenght = len(pin)
+
+if pin.isdigit():
+    if (pin_lenght >4):
+        print("please use 4 nummbers")
+        
+    
+    else:
+        print(pin)
+
+
+else:
+    print("use only nummbers")
+
+
+
+
+if pin.isdigit():
+
+
+    if (pin_lenght <4):
+        print("please use 4 nummbers")
+
+
+    else:
+        print(pin)
+
+
+else:
+    print("use only nummbers")
