e1d94613fc9b4b192c466c2e691f45be623672e4
ElisBjorkman <128683649+ElisBjorkman@users.noreply.github.com>
2023-12-05 10:02:15 +0100

    Add files via upload

---
+++ magic8ball.py
@@ -0,0 +1,24 @@
+import random
+print("Skriv en fråga så kommer magic 8 ball ge dig ett svar!")
+input()
+
+Ja_lista = ("Ja", "Absolut", "självklart", "Untan Tvekan")
+
+Nej_lista = ("Absolut Inte", "Nej Inte En Chans", "Nej", "Dra Åt HELVETE!!!")
+
+kancke_lista = ("Det Kan Vara Möjligtvis", "OSäker", "Kancke")
+
+
+result = random.randint(1, 3)
+
+if result == 1:
+    print(random.choice(Ja_lista))
+
+elif result == 2:
+    print(random.choice(kancke_lista))
+
+else result == 3:
+    print(random.choice(Nej_lista))
+
+
+
