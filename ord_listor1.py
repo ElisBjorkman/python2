83cbe6cb0b7a001332142fa9aa0e556c9dcebe13
Elis Björkman <128683649+ElisBjorkman@users.noreply.github.com>
2024-02-06 10:11:57 +0100

    Add files via upload

---
+++ ord_listor1.py
@@ -0,0 +1,9 @@
+
+list1 = ["Hej", "På", "Dig"]
+list2 = []
+
+for i in range(len(list1)):
+
+    list2.append(len(list1[i]))
+
+print(list2)
---
+++ ord_listor2.py
@@ -0,0 +1,11 @@
+
+
+list1 = ["He", "P", "D"]
+list2 = ["j", "å", "ig"]
+list3 = []
+
+for i in range(len(list1)):
+
+    list3.append(list1[i] + list2[i])
+
+print(list3)
