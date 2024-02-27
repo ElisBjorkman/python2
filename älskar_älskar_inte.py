9c576efb7dc593459fc910f9d3a9ec1b0e0d6d06
Elis Björkman <128683649+ElisBjorkman@users.noreply.github.com>
2024-02-27 11:29:13 +0100

    Add files via upload

---
+++ älskar_älskar_inte.py
@@ -0,0 +1,15 @@
+print("älskar älskar inte")
+print("sätt in hur många blad bloman har!")
+
+blad = int(input())
+
+for i in range (blad):
+    
+    if i %2==0:
+        #stämer
+        print("älskar")
+    else:
+        #stämer inte
+        print("älskar inte")
+
+
