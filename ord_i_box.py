3183db0fc42b413f9b4010e0e49a94dada4ab82d
Elis Björkman <128683649+ElisBjorkman@users.noreply.github.com>
2024-04-16 11:32:01 +0200

    Add files via upload

---
+++ ord_i_box.py
@@ -0,0 +1,28 @@
+
+
+
+ordet = input()
+
+längden_av_ordet = len(ordet)
+
+
+
+mellanrum = 2
+
+box = ""
+box = "#" (längden_av_ordet + (mellanrum*2) + (1*2))
+box = "\n"
+
+"""
+#########
+#       #
+#  hej  #
+#       #
+#########
+
+
+längden_av_ordet + (mellanrum*2) + (1*2)
+"""
+
+
+print(box)
